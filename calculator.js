9cee7ba7711f38bb965717c42911911ae31cf2d9
Donovan Burk <mac6@129mac18.valleychristian.net>
2017-01-06 11:02:12 -0800

    add calc functions

--- calculator.js
+++ calculator.js
@@ -2,12 +2,20 @@
 
 // Change Display
 function d(val) {
+		document.getElementById("d").value = val;
 }
 
 // Type numbers and operators
 function v(val) {
+		document.getElementById("d").value += val;
 }
 
 // Evaluate the equation
-function c() {
+function e() {
+		try {
+				d(eval(document.getElementById("d").value));
+		}
+		catch(err) {
+				d("E3r");
+		}
 }
