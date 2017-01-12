51b70a80becedc53d361d91dc2a3b59d3b6faf29
Donovan Burk <mac6@129mac18.valleychristian.net>
2017-01-12 10:21:20 -0800

    made display say zero
    added an edited variable

--- calculator.js
+++ calculator.js
@@ -1,13 +1,24 @@
 // JavaScript Document
 
 // Change Display
+var edit = 0;
 function d(val) {
-		document.getElementById("d").value = val;
+		if(val == '') {
+				edit = 0;
+				document.getElementById("d").value = 0;
+		}else{
+				document.getElementById("d").value = val;
+		}
 }
 
 // Type numbers and operators
 function v(val) {
-		document.getElementById("d").value += val;
+		if(edit == 0){
+				edit = 1;
+				document.getElementById("d").value = val;
+		}else{
+				document.getElementById("d").value += val;
+		}
 }
 
 // Evaluate the equation
