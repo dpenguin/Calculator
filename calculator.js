b4fd568234bf0ab0c47343f818bad301812a6a13
Donovan Burk <mac6@129mac18.valleychristian.net>
2017-01-12 11:22:04 -0800

    memory input functions

--- calculator.js
+++ calculator.js
@@ -5,12 +5,14 @@ var edit = 0;
 var deci = 0;
 var oper = 0;
 var rroper = 0;
+var er = 0;
 function d(val) {
 		if(val == '') {
 				edit = 0;
 				deci = 0;
 				oper = 0;
 				rroper = 0;
+				er = 0;
 				document.getElementById("d").value = 0;
 		}else{
 				document.getElementById("d").value = val;
@@ -22,6 +24,7 @@ function v(val) {
 		if(val == '/' || val == '*' || val == '-' || val == '+'){
 				if(rroper === 1){
 						d("ErRoar too many operators");
+						er = 1;
 				}else{
 						document.getElementById("d").value += val;
 						rroper = 1;
@@ -29,6 +32,7 @@ function v(val) {
 		}else if(val === '.') {
 				if(deci === 1){
 						d("ErRoar too many decimals");
+						er=1;
 				}else{
 						document.getElementById("d").value += val;
 						deci = 1;
@@ -50,15 +54,28 @@ function m(val){
 		if(val =='rc') {
 				if(ismem = 0) {
 						d("ErRoar you have no value saved");
+						er = 1;
 				}else if(rroper == 1){
 						document.getElementById("d").value += mem;
 				}else{
 						document.getElementById("d").value = mem;
 				}
 		}else if(val =='-') {
-				
+				if(oper =>1){
+						d("ErRoar you cannot save an expression");
+				}else if(er = 1){
+						d("ErRoar you cannot save an error message");
+				}else{
+						mem = (-1)*(Number(document.getElementById("d").value));
+				}
 		}else{
-				
+				if(oper =>1){
+						d("ErRoar you cannot save an expression");
+				}else if(er = 1){
+						d("ErRoar you cannot save an error message");
+				}else{
+						mem = Number(document.getElementById("d").value);
+				}
 		}
 }
 
